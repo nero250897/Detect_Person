3c0fce0760ee25c13ccd015531508b27cbd620a7
nero250897 <53467404+nero250897@users.noreply.github.com>
2023-07-09 18:16:18 +0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,38 +1,37 @@
-from ultralytics import YOLO
 import cv2
-import math
+from ultralytics import YOLO
 import numpy as np
+import math
 import time
-#import threading
 
-cap = cv2.VideoCapture("C:/Users/84335/PycharmProjects/Doan/Main3.mp4")
+model = YOLO("best.pt")
+classNames = ["person"]
+
+cap = cv2.VideoCapture("C:/Users/84335/PycharmProjects/Main_yolov8/Main3.mp4")
+
+# Khoi tao bien de tinh toan FPS
+start_time = time.time()
+frame_count = 0
 
 # Mang chua cac diem de ve da giac
 polygons = [
     np.array([[248, 278],[488, 270],[552, 386],[160, 386],[236, 290]], np.int32)
     ]
 
-model = YOLO("best.pt")
-classNames = ["person"]
-
-# Khoi tao bien de tinh toan FPS
-start_time = time.time()
-frame_count = 0
-
 while True:
     success, img = cap.read()
-    results = model(img, stream=True)
+    results = model(img, stream=True, device='0') # chay tren GPU
 
     # Ve cac da giac len anh
     for polygon in polygons:
-        cv2.polylines(img, [polygon], isClosed=True, color=(0, 0, 255), thickness=3) # Mau do
+        cv2.polylines(img, [polygon], isClosed=True, color=(0, 0, 255), thickness=2) # Mau do
 
     for r in results: # Vong lap hien thi bouding box cho moi ket qua
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3) #Mau xanh
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 1) #Mau xanh
 
             # Tinh toan centroid
             centroid = ((x1 + x2) // 2, (y1 + y2) // 2)
@@ -50,31 +49,29 @@ while True:
             # print(t_size)
             c2 = x1 + t_size[0], y1 - t_size[1] - 3
             cv2.rectangle(img, (x1, y1), c2, [255, 0, 0], -1, cv2.LINE_AA)  # filled Mau xanh la
-            cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA) #Mau trang
+            cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)  # Mau trang
 
-    # Kiem tra doi tuong co trong khu vuc polygon hay khong
-    is_inside = cv2.pointPolygonTest(polygon, tuple(centroid), False)
+            # Kiem tra doi tuong co trong khu vuc polygon hay khong
+            is_inside = cv2.pointPolygonTest(polygon, tuple(centroid), False)
 
-    if is_inside >= 0:
-        text = "Co xam nhap"
-        # Hien canh bao len man hinh
-        cv2.putText(img, "Canh bao: {}".format(text), (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        # In anh
-        cv2.imwrite("alert.png", img) #cv2.resize(img, dsize=None, fx=0.2, fy=0.2
+            if is_inside >= 0:
+                text = "Co xam nhap"
+                # Hien canh bao len man hinh
+                cv2.putText(img, "Canh bao: {}".format(text), (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                # In anh
+                cv2.imwrite("alert.png", img)
 
-    # Tinh toan FPS
-    frame_count += 1
-    elapsed_time = time.time() - start_time
-    fps = frame_count / elapsed_time
-    cv2.putText(img, f"FPS: {round(fps, 2)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # Tinh toan FPS
+        frame_count += 1
+        elapsed_time = time.time() - start_time
+        fps = frame_count / elapsed_time
+        cv2.putText(img, f"FPS: {round(fps, 2)}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
-    resized_frame = cv2.resize(img, (960, 540))
-    cv2.imshow("Image", resized_frame)
+    resized_frame = cv2.resize(img, (1000, 640))
+    cv2.imshow("Object Detection", resized_frame)
 
 cap.release()
 cv2.destroyAllWindows()
-
-
